755b67e7dceb298b16cd21070cf48bfe78c1e49b
kff11 <55500009+kff11@users.noreply.github.com>
2020-07-03 19:43:23 +0900

    K번째 수

---
+++ src/Chapter1/K.java
@@ -0,0 +1,27 @@
+package Chapter1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class K {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(solution(array, commands)[0]);
+        System.out.println(solution(array, commands)[1]);
+        System.out.println(solution(array, commands)[2]);
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
